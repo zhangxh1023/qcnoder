8d4d11553aba6ee5ca5f5dc4a99f7c2a61b87234
zhangxh1023 <zhangxh1023@qq.com>
2022-09-28 17:12:49 +0800

    Initial Commit

---
+++ qcnoder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  qcnoder
+//
+//  Created by zhangxh1023 on 2022/9/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ qcnoder/qcnoderApp.swift
@@ -0,0 +1,17 @@
+//
+//  qcnoderApp.swift
+//  qcnoder
+//
+//  Created by zhangxh1023 on 2022/9/28.
+//
+
+import SwiftUI
+
+@main
+struct qcnoderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
